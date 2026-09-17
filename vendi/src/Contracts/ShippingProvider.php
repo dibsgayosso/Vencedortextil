e04c4a64c78150d177d1ff61d9cea71a88cc2ffd
Dibs <dibsjimenez@gmail.com>
2026-09-17 17:53:31 -0600

    feat(vendi): add vendi/src/Contracts/ShippingProvider.php

---
+++ vendi/src/Contracts/ShippingProvider.php
@@ -0,0 +1,3 @@
+<?php declare(strict_types=1);
+namespace Vendi\Contracts;
+interface ShippingProvider { public function quote(array $shipment): array; public function createLabel(array $shipment,string $serviceId): array; public function track(string $trackingNumber): array; }
